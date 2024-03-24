41dee9036b731f412bbe5a33f4875222ada193fa
Vladut <stanescuvladutgeorge@gmail.com>
2024-03-24 18:58:57 +0200

    Footer changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
     <>
-      <div className="card footer-container">
-        <div className="card-body text-center text-white bg-secondary">
+      <div className="card footer-container fixed-bottom rounded-5">
+        <div className="card-body text-center text-white bg-black rounded-5">
           <p className="card-text">
-            Copyright 2024, Licență de{" "}
+            Licență de{" "}
             <a href="https://github.com/stnsc" className="link-info">
               Stănescu Vlăduț-George
             </a>
